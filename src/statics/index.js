cf23f53cc4c9b1525bc7273996bd669fc8dc8ad2
Azeem Akhter <azeem.akhter@futurice.com>
2019-10-23 14:32:38 +0300

    Remove API URL From the static file

--- src/statics/index.js
+++ src/statics/index.js
@@ -1,5 +1,3 @@
-export const API_BASE_URL = "http://ui-developer-backend.herokuapp.com/api";
-
 export const ENDPOINTS = {
   conversations: () => `/conversations`,
   messages: conversationID => `/conversations/${conversationID}/messages`,
